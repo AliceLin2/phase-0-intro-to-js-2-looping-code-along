f2bd9a81fee63318a3d02d7993f8855df59f5c62
shu Lin <bbqlinshu@gmail.com>
2021-11-09 00:45:23 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(nameArray, gift){
+    let msgArray=[];
+    for (let i=0; i<nameArray.length; i++){
+        msgArray.push(`Thank you, ${nameArray[i]}, for the wonderful ${gift} gift!`);
+    }
+    return msgArray;
+}
+
+function countDown(number){
+    while(number>=0){
+        console.log(number);
+        number--;
+    }
+
+}
